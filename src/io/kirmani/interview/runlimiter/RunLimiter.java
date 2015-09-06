39c0ac421b537ef678645ae57501606ab33ca6c1
Sean Kirmani <sean@kirmani.io>
2015-09-06 20:36:20 +0000

    Changed RunLimiter

--- src/io/kirmani/interview/runlimiter/RunLimiter.java
+++ src/io/kirmani/interview/runlimiter/RunLimiter.java
@@ -8,16 +8,39 @@
 package io.kirmani.interview.runlimiter;
 
 import java.lang.System;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class RunLimiter {
 	private static final int LIMIT = 100;
     private static final long NANO_IN_SEC = 1000000000;
 
-    private static long startTime = 0;
-    private static int count = 0;
+    private long mStartTime;
+    private int mCount;
 
-    public static boolean canExec() {
+    private Queue<Long> mTimes;
+
+    public RunLimiter() {
+        mStartTime = 0;
+        mCount = 0;
+        mTimes = new LinkedList<Long>();
+    }
+
+    public boolean canExec() {
         long currentTime = now();
+        if (mTimes.size() == 0) {
+            mTimes.add(currentTime);
+            return true;
+        }
+        while (currentTime - mTimes.peek() >= NANO_IN_SEC) {
+            mTimes.remove();
+        }
+        if (mTimes.size() >= LIMIT) {
+            return false;
+        }
+        mTimes.add(currentTime);
+        return true;
+        /*
         if (currentTime - startTime < NANO_IN_SEC) {
             if (count >= LIMIT) {
                 return false;
@@ -28,6 +51,7 @@ public class RunLimiter {
             startTime = currentTime;
         }
         return true;
+        */
     }
 
     private static long now() {
--- src/io/kirmani/interview/runlimiter/RunLimiterTest.java
+++ src/io/kirmani/interview/runlimiter/RunLimiterTest.java
@@ -9,13 +9,19 @@ package io.kirmani.interview.runlimiter;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class RunLimiterTest {
     private static final long NANO_IN_SEC = 1000000000;
 
-    @Test
-    public void testNothing() {}
+    private RunLimiter mRunLimiter;
+
+    @Before
+    public void initialize() {
+        mRunLimiter = new RunLimiter();
+    }
 
     @Test
     public void testMeetsSpecInFirstSecond() {
@@ -23,7 +29,7 @@ public class RunLimiterTest {
         int[] timesTrue = new int[] {0};
         long[] startTime = new long[] {now()};
         executeOverInterval(100, NANO_IN_SEC, timesExecuted, timesTrue, startTime);
-        assertEquals("CanExec() should have executed 100 times.", 100, timesTrue[0]);
+        assertEquals("CanExec() should have been true 100 times.", 100, timesTrue[0]);
     }
 
     @Test
@@ -46,11 +52,16 @@ public class RunLimiterTest {
                 100, timesExecuted[0]);
         assertEquals("CanExec() should have been true 100 times at this point.",
                 100, timesTrue[0]);
-        executeOverInterval(190, NANO_IN_SEC * 12 / 10, timesExecuted, timesTrue, startTime);
+        executeOverInterval(190, NANO_IN_SEC * 11 / 10, timesExecuted, timesTrue, startTime);
         assertEquals("CanExec() should have been executed 190 times at this point.",
                 190, timesExecuted[0]);
-        /* assertEquals("CanExec() should have been true 100 times at this point.",
-                100, timesTrue[0]); */
+        assertEquals("CanExec() should have been true 100 times at this point.",
+                100, timesTrue[0]);
+        executeOverInterval(200, NANO_IN_SEC * 19 / 10, timesExecuted, timesTrue, startTime);
+        assertEquals("CanExec() should have been executed 200 times at this point.",
+                200, timesExecuted[0]);
+        assertEquals("CanExec() should have been true 110 times at this point.",
+                110, timesTrue[0]);
     }
 
     private void executeOverInterval(int endTimesExecuted, long endTime /* nanoseconds */,
@@ -61,7 +72,7 @@ public class RunLimiterTest {
         int startTimesExecuted = timesExecuted[0];
         while (now() - startTime[0] < endTime) {
             if (timesExecuted[0] < endTimesExecuted) {
-                if (RunLimiter.canExec()) {
+                if (mRunLimiter.canExec()) {
                     timesTrue[0]++;
                 }
                 timesExecuted[0]++;
